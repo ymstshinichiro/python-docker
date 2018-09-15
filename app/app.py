21410af842c526f347849bf1a6c54ea35c0f0270
shinichiroYmst <yamashitashinichirou@gmail.com>
2018-09-15 13:03:59 +0900

    コードをシンプル化

--- app/app.py
+++ app/app.py
@@ -2,29 +2,29 @@ import json
 import xmltodict
 
 def main():
-    json_text = file_read('../json/data.json')
-    json_data = json.loads(json_text)
+    json_data = json.loads(file_read('../json/data.json'))
+    xml_data = xmltodict.parse(file_read('../xml/data.xml'))
 
-    xml_text = file_read('../xml/data.xml')
-    xml_data = xmltodict.parse(xml_text)
+    print_with_header('-- json_data ---- ', json_data)
+    print_with_header('-- xml_data ---- ', xml_data)
 
-    print('-- json_data --------------')
-    print(json_data)
-    print('-- xml_data --------------')
-    print(xml_data)
-
-    print('\n-- json_data["employees"] --------------')
-    for emp in json_data['employees']:
-        print(emp['name'])
-
-    print('-- xml_data["root"]["employees"] --------------')
-    for emp in xml_data['root']['employees']:
-        print(emp['name'])
-
+    print_foreach_with_header('-- json_data["employees"] -- ',  json_data['employees'], 'name')
+    print_foreach_with_header('-- xml_data["root"]["employees"] -- ',  xml_data['root']['employees'], 'name')
 
 def file_read(path):
     with open(path, 'r') as file:
         text = file.read()
     return text
+
+def print_with_header(headertext, printobject):
+    print(headertext)
+    print(printobject)
+    print()
+
+def print_foreach_with_header(headertext, printobjects, printlabel):
+    print(headertext)
+
+    for obj in printobjects:
+        print(obj[printlabel])
 
 main()
